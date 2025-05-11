4994c3e102a3b9bb5a617371b645e57f17ed0730
eljorgecruz <jcruz42@uco.mx>
2025-05-10 20:39:29 -0600

    First crud api controller using entity framework

--- WebApiPerson/Program.cs
+++ WebApiPerson/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiPerson.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Add var for string connection
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+// Add service for the connection
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(connectionString)
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
